c5b29a0a903a24a38fb356aa8591223d7fbeecdf
Zeng Yi <15558168773@163.com>
2016-04-26 23:14:11 +0800

    用django搭建完网站，并显示成功显示首页。

--- BlogSys/Blog/views.py
+++ BlogSys/Blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.template import Template
+from django.http import HttpResponse
 
-# Create your views here.
+def Home(request):
+    #index = Template("IndexBase.html")
+    return HttpResponse(render(request, "IndexBase.html"))
